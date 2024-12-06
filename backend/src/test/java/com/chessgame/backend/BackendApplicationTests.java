452e9d2cdead5d6fc16799d9272e54012887f030
Justinj68 <justinjds68@gmail.com>
2024-12-06 15:07:45 +0100

    init backend

---
+++ backend/src/test/java/com/chessgame/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chessgame.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
